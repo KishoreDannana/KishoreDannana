8069d4ca245ed6a73c9483d3b857e350c5452284
KishoreDannana <109733779+KishoreDannana@users.noreply.github.com>
2022-08-29 20:00:43 +0530

    Create 29-08-2022 nth.c

---
+++ 29-08-2022 nth.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+int find_nth_term(int n, int a, int b, int c) {
+  int ans2,i;
+  for(i=4;i<=n;i++)
+  {
+      ans2=a+b+c;
+      a=b;
+      b=c;
+      c=ans2;
+  }
+  return ans2;
+}
+
+int main() {
+    int n, a, b, c;
+  
+    scanf("%d %d %d %d", &n, &a, &b, &c);
+    int ans = find_nth_term(n, a, b, c);
+ 
+    printf("%d", ans); 
+    return 0;
+}
